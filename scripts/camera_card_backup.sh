10feb44d7be4bd4f6a553d3b8b5c5590d2ccacbe
st599 <st599@users.noreply.github.com>
2015-07-13 11:46:41 +0100

    Added MD5SUM checks to camera_card_backup.sh

--- scripts/camera_card_backup.sh
+++ scripts/camera_card_backup.sh
@@ -5,7 +5,7 @@
 #	date		:	13/07/2015
 #	version		:	0.1 - Rsync
 #			:	
-#	requires	:	rsync
+#	requires	:	rsync, md5sum
 #	copyright	:	(c) st599 MMXV
 #
 #######################################################################################
@@ -16,6 +16,7 @@ echo ""
 echo " (c) st599 ver 0.1"
 echo ""
 echo "usage camera_card_backup.sh <input_directory>"
+
 
 #####################
 # INITIALISE        #
@@ -33,12 +34,24 @@ OUTPUT_DIR=~/camera_backup/`TZ='Europe/London' date +%y%m%d%H%M`/
 echo -n "......Output Directory: "
 echo $OUTPUT_DIR
 
+
 #####################
 # COPY              #
 #####################
 
 echo "...Copying"
-
 rsync -uvh --progress $INPUT_DIR $OUTPUT_DIR
-
 echo "......Copied"
+
+
+#####################
+# CHECK             #
+#####################
+
+cd $IMPUT_DIR
+
+for file in *
+do
+	echo "in: " `md5sum -cb $file` "  out: " `md5sum -cb $OUTPUT_DIR/$file`
+done
+
