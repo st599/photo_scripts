9e9941174ce98866766a73bda82aaabd913ecfab
st599 <st599@users.noreply.github.com>
2015-07-13 10:24:36 +0100

    Updated to astro_star_stacking.sh to allow subjective assessment of mean and median filters:
    blackframe     starframe
    mean		mean
    mean		median
    median		mean
    median 		median
    
    this fixes #3

--- scripts/astro_star_stacking.sh
+++ scripts/astro_star_stacking.sh
@@ -37,6 +37,7 @@ done
 echo "......Average Black Frames"
 
 convert *.tiff -alpha off -evaluate-sequence mean -depth 16 blackframe.tiff
+convert *.tiff -alpha off -evaluate-sequence median -depth 16 blackframe1.tiff
 
 echo "......Move Black Frame"
 
@@ -63,24 +64,34 @@ echo "......Convert CR2 to Linear TIFF and Subtract Black File"
 for file in *.CR2
 do
 	dcraw -T -4 $file
-	composite -compose minus ${file%.CR2}.tiff blackframe.tiff -depth 16 corr_${file%.CR2}.tiff
+	composite -compose minus ${file%.CR2}.tiff blackframe.tiff -depth 16 corr1_${file%.CR2}.tiff
+	composite -compose minus ${file%.CR2}.tiff blackframe1.tiff -depth 16 corr2_${file%.CR2}.tiff
 done
 
 echo "......Align Image Stack"
 
-fl='corr*.tiff'
+fl='corr1*.tiff'
 
-align_image_stack -v -a ais $fl
+align_image_stack -v -a ais1 $fl
+
+fl='corr2*.tiff'
+
+align_image_stack -v -a ais2 $fl
 
 echo "......Average Corrected Star Frames"
 
-convert ais*.tif -alpha off -evaluate-sequence mean -depth 16 final_stars.tiff
+convert ais1*.tif -alpha off -evaluate-sequence mean -depth 16 final_stars1.tiff
+convert ais1*.tif -alpha off -evaluate-sequence median -depth 16 final_stars2.tiff
+convert ais2*.tif -alpha off -evaluate-sequence mean -depth 16 final_stars3.tiff
+convert ais2*.tif -alpha off -evaluate-sequence median -depth 16 final_stars4.tiff
 
 echo "......Convert to sRGB"
 
-convert final_stars.tiff -set colorspace RGB -colorspace sRGB -depth 16 final_stars.tiff
-
-mv final_stars.tiff ../
+convert final_stars1.tiff -set colorspace RGB -colorspace sRGB -depth 16 final_stars1.tiff
+convert final_stars2.tiff -set colorspace RGB -colorspace sRGB -depth 16 final_stars2.tiff
+convert final_stars3.tiff -set colorspace RGB -colorspace sRGB -depth 16 final_stars3.tiff
+convert final_stars4.tiff -set colorspace RGB -colorspace sRGB -depth 16 final_stars4.tiff
+mv final_stars*.tiff ../
 
 rm *.tiff
 
